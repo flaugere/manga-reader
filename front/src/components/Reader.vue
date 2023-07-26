a9e33022c547bb3e1bc4467c0761a4b42a98d8ef
DAVID Flaugere <flaugere.david@gmail.Com>
2023-07-26 11:51:33 +0200

    Revert "Menu de sélection du volume"

--- front/src/components/Reader.vue
+++ front/src/components/Reader.vue
@@ -8,20 +8,11 @@ let groupToPrint : Group = ref(null);
 </script>
 
 <template>
-  <!-- <li class="title-select" v-for="group in props.comic.groups" :key="group.name">
-    <a :href="comic.groups">
-      <div style="display: flex; align-content: center;"><img :src="comic.logo" style="float: left ;max-width: 90px; padding-right: 10px;"/> {{ comic.name }} : {{ group.name }}  </div>
-    </a>
-  </li> -->
   <div class="container mx-auto px-10">
     <h1>{{ props.comic.name }}</h1>
-    <img :src="props.comic.image" />
-  </div>
-  <div class="container mx-auto px-10">
-    <div class="title-select" v-for="group in props.comic.groups" @click.prevent="groupToPrint = group">
+    <div v-for="group in props.comic.groups" @click.prevent="groupToPrint = group">
       {{ group.name }}
     </div>
-    <img v-if="groupToPrint?.name" :src="groupToPrint?.pages[0].image" />
-    <!-- <comic-group v-if="groupToPrint?.name" :group="groupToPrint" /> -->
+    <comic-group v-if="groupToPrint?.name" :group="groupToPrint" />
   </div>
 </template>
